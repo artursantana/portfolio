51c99d2e288c0ff51727a5ad9abd9cba351e9f3e
artursantana <artursantana843@gmail.com>
2023-12-31 17:18:54 +0000

    first commit

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,7 +3,10 @@ import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Project01 from '../images/githubfinder.png'
-import Project02 from '../images/pro.png'
+import Project02 from '../images/cost.png'
+
+import tolls01 from '../images/reacttype.png'
+import tolls02 from '../images/react.png'
 
 // Import Swiper styles
 import 'swiper/css';
@@ -29,12 +32,14 @@ const Projects = () => {
     >
       
       <SwiperSlide>
-        <img src={Project01} alt="" />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis odio fugiat, libero aliquid, amet deserunt tempora qui illum magnam iste possimus, hic fuga similique odit reiciendis tenetur ipsa dolorem perferendis!
+        <a href="https://github.com/artursantana/Search-GitHub/tree/main/githubFinder" target='blank'><img className='imgProject' src={Project01} alt="" /></a>
+        <div>This project is a search engine on GITHUB, made with the tools below</div>
+        <img className='imgTools' src={tolls01} alt="" />
       </SwiperSlide>
       <SwiperSlide>
-      <img src={Project02} alt="" />
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis odio fugiat, libero aliquid, amet deserunt tempora qui illum magnam iste possimus, hic fuga similique odit reiciendis tenetur ipsa dolorem perferendis!
+      <a href="https://github.com/artursantana/Costs" target='blank'><img className='imgProject' src={Project02} alt="" /></a>
+      <div>This project is a search engine on GITHUB, made with the tools below</div>
+        <img className='imgTools' src={tolls02} alt="" />
       </SwiperSlide>
     </Swiper>
   )
